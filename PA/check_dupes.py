2c3e86480555e6f0017b849dab6c8f39a9c809f4
Ubuntu <ubuntu@ip-172-31-24-95.us-west-1.compute.internal>
2020-11-02 03:31:50 +0000

    correct headers as per the PA docs

--- PA/check_dupes.py
+++ PA/check_dupes.py
@@ -6,15 +6,15 @@ import pandas as pd
 import os
 import re
 
-DATA_DIR = '/home/ubuntu/edata/PA'
+DATA_DIR = '/edata/PA'
 
-HEADER_FILE = '/home/ubuntu/edata/PA/header.txt'
+HEADER_FILE = '/edata/PA/headers.txt'
 
 with open(HEADER_FILE, 'r') as file:
     headers = file.read().split(',')
 
 for filename in os.listdir(DATA_DIR):
     if re.search('FVE', filename):
-        df = pd.read_csv(filename, names=headers)
+        df = pd.read_csv("{}/{}".format(DATA_DIR, filename), names=headers, sep='\t')
         import pdb; pdb.set_trace()
 
